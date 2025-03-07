8854beebc4a515d597a5e44eb390ab6aec42f171
dlstlr1707 <dlstlr1707@naver.com>
2025-03-07 16:40:49 +0900

    1st generate

---
+++ toby-spring-act1/src/main/java/com/example/tobyspringact1/TobySpringAct1Application.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringact1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringAct1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringAct1Application.class, args);
+    }
+
+}
